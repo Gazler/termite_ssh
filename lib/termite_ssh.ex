82b21e63ce251718fe9d5fab88ac15bb425af295
Gary Rennie <gazler@gmail.com>
2026-03-18 10:03:40 +0000

    chore: initial setup

---
+++ lib/termite_ssh.ex
@@ -0,0 +1,18 @@
+defmodule TermiteSsh do
+  @moduledoc """
+  Documentation for `TermiteSsh`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TermiteSsh.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/termite_ssh_test.exs
@@ -0,0 +1,8 @@
+defmodule TermiteSshTest do
+  use ExUnit.Case
+  doctest TermiteSsh
+
+  test "greets the world" do
+    assert TermiteSsh.hello() == :world
+  end
+end
